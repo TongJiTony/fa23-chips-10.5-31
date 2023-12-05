c2681d2fafef336ecf21ed76d3987d411f0dbe43
Zixun Huang <tonyhuang@berkeley.edu>
2023-12-05 04:25:35 +0000

    merge two pages

--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,6 @@ Rails.application.routes.draw do
     
     resources :representatives do
         resources :news_items, only: %i[index show]
-        get '/representatives/:representative_id/my_news_item/new/search' => 'my_news_items#search',
-            :as => :search_news_item_with_rep_issue
         get '/representatives/:representative_id/my_news_item/new' => 'my_news_items#new',
             :as                                                    => :new_my_news_item
         match '/representatives/:representative_id/my_news_item/new', to:  'my_news_items#create',
@@ -42,6 +40,8 @@ Rails.application.routes.draw do
                                                                       via: %i[put patch]
         match '/representatives/:representative_id/my_news_item/:id', to:  'my_news_items#destroy',
                                                                       via: [:delete]
+        get '/representatives/:representative_id/my_news_item/new/search' => 'my_news_items#search',
+            :as                                                    => :search_news_item_with_rep_issue
     end
     get '/search/(:address)' => 'search#search', :as => 'search_representatives'
 end
